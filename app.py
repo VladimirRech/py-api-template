95be1b0ac45d128c15f9bb919c41fd7cb29d3c9d
Vladimir Rech <rechvladimir@hotmail.com>
2021-04-05 21:55:11 -0300

    Adicionando método POST.

--- app.py
+++ app.py
@@ -14,6 +14,31 @@ class Users(Resource):
         data = data.to_dict()  # convert dataframe to dictionary
         return {'data': data}, 200  # return data and 200 OK code.
 
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize
+
+        parser.add_argument('userId', required=True)
+        parser.add_argument('name', required=True)
+        parser.add_argument('city', required=True)
+
+        args = parser.parse_args()  # parse argumentos to dictionary
+
+        # create new data frame containing new values
+        new_data = pd.DataFrame({
+            'userId': args['userId'],
+            'name': args['name'],
+            'city': args['city'],
+            'locations': [[]]
+        })
+
+        # read our CSV
+        data = pd.read_csv('./csv/users.csv')  # read the csv file
+        # add the newly provided values
+        data = data.append(new_data, ignore_index=True)
+        # save back to CSV
+        data.to_csv('./csv/users.csv', index=False)
+        return {'data': data.to_dict()}, 200  # return data with 200 OK
+
 
 class Locations(Resource):
     pass
