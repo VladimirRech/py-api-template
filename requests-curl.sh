b046158b5f61c5f94815dccd364702c30c74a07f
Vladimir Rech <rechvladimir@hotmail.com>
2021-04-06 22:08:01 -0300

    Novos modelos para requisições com CURL.

--- requests-curl.sh
+++ requests-curl.sh
@@ -2,4 +2,8 @@
 curl http://127.0.0.1:5000/users
 
 # post new user
-curl -d '{ "userId": "321", "name": "Vlad", "city": "Transilvania" }' http://127.0.0.1:5000/users
+
+# JSON
+curl -d '{ "userId": "322", "name": "Wayne", "city": "Gothan" }' -H 'Content-Type: application/json' -X POST http://127.0.0.1:5000/users
+# WITH PARAMETERS
+curl -d "userId=321&name=Vlad&city=Transilvania" -X POST http://localhost:5000/users
