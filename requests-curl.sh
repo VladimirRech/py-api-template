4376ed1b0a9133855c1a898154b19c99dcccd385
Vladimir Rech <rechvladimir@hotmail.com>
2021-04-12 08:29:32 -0300

    Adicionando request para método DELETE.

--- requests-curl.sh
+++ requests-curl.sh
@@ -7,3 +7,9 @@ curl http://127.0.0.1:5000/users
 curl -d '{ "userId": "322", "name": "Wayne", "city": "Gothan" }' -H 'Content-Type: application/json' -X POST http://127.0.0.1:5000/users
 # WITH PARAMETERS
 curl -d "userId=321&name=Vlad&city=Transilvania" -X POST http://localhost:5000/users
+
+# put location
+curl -d "userId=321&location=0007" -X PUT http://localhost:5000/users
+
+# delete
+curl -d "userId=321" -X DELETE http://localhost:5000/users
